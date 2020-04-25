7da2fed380f2dbd770e4d8fa55a8b2ceb6318d6a
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-04-25 00:20:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(board_array)
   ii=0 
   while(true)
-    puts(" #{board_array[ii} | #{board_array[ii+1} | #{board_array[ii+2}"")
+    puts(" #{board_array[ii} | #{board_array[ii+1} | #{board_array[ii+2]}")
     ii +=3
     if(ii==9)
       break
