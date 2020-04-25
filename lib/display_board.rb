056b1425c609ce7f672b84ef60b7030e32bf6020
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-04-25 00:21:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board_array)
+  ii=0 
+  while(true)
+    puts(" #{board_array[ii]} | #{board_array[ii+1]} | #{board_array[ii+2]} ")
+    ii +=3
+    if(ii==9)
+      break
+    else 
+      puts "-----------"
+    end
+  end
+end
